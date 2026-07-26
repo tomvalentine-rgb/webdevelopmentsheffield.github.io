c5c0d4962a6f2c11d32efa2d0837047256b8f20f
tom3636 <tom@pyng.uk>
2026-07-26 10:28:28 +0100

    add web design page

--- scripts/build-blog.mjs
+++ scripts/build-blog.mjs
@@ -487,7 +487,7 @@ ${body}
 
 <section class="article-cta-wrapper">
   <section class="article-cta">
-      <h2>Need a Website for Your Business?</h2>
+      <h2>Need a website for your business?</h2>
       <p> We partner with businesses at every stage of growth to deliver custom websites that drive real results. </p>
       <a href="/index.html#contact" class="btn-primary">
           Get a Quote
